964903ba833cc5a657228bdb130631bc6e378af2
Mathieujaz <mathieujaz17@gmail.com>
2025-02-21 16:10:12 -0500

    Organized code, nothing added

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginRegisterPage from "./LoginRegisterPage.jsx"; 
 import "./index.css";
 import Dashboard from "./Dashboard.jsx";
@@ -9,7 +10,7 @@ function App() {
     <div className="app-container">
       <AuthProvider>
       <LoginRegisterPage />
-      {/* <Dashboard /> */}
+      {/*<Dashboard />*/}
       </AuthProvider>
     </div>
   );
