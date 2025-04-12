b505e3fbcd97fc16725183428d4492cf98237919
wassdak <wassimodakka@hotmail.ca>
2025-04-12 13:32:01 -0400

    Clarify where specs are updating in config file

--- react-app/cypress.config.js
+++ react-app/cypress.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
-    supportFile: false, // Disable the default support file lookup if not needed
+    supportFile: false, // if you don't need a support file
+    specPattern: 'react-app/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
 });
