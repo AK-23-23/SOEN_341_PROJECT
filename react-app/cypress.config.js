b5dc71267a9b5a79dfcc3b0fb773ca7121891919
wassdak <wassimodakka@hotmail.ca>
2025-04-12 13:24:44 -0400

    Disable the default cypress support file

--- react-app/cypress.config.js
+++ react-app/cypress.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173'
+    baseUrl: 'http://localhost:5173',
+    supportFile: false, // Disable the default support file lookup if not needed
   },
 });
